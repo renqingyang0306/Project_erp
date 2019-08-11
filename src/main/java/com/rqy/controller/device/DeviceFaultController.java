137ef4d3820fb699c88a3abe3cda8d1300ba2de4
Mprotecter <50576329+Mprotecter@users.noreply.github.com>
2019-08-11 09:09:58 +0800

    ly note

--- src/main/java/com/rqy/controller/device/DeviceFaultController.java
+++ src/main/java/com/rqy/controller/device/DeviceFaultController.java
@@ -103,7 +103,18 @@ public class DeviceFaultController {
             return RespondMsg.createSusscess();
         }
         return  RespondMsg.createFail();
+    }  
+    @ResponseBody
+    @RequestMapping("deviceFault/update_note")
+    public RespondMsg updateall(DeviceFault device) {
+        //插入用领域模型
+        int i = deviceFaultService.updateByPrimaryKey(device);
+        if (i==1){
+            return RespondMsg.createSusscess();
+        }
+        return  RespondMsg.createFail();
     }
+    
     //增
     @ResponseBody
     @RequestMapping("deviceFault/insert")
