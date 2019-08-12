1673aa52701f7f73b28e088c730f3b9125eb7663
Mprotecter <50576329+Mprotecter@users.noreply.github.com>
2019-08-12 15:53:16 +0800

    ly 增加validation输入后端验证

--- src/main/java/com/rqy/controller/device/DeviceContorller.java
+++ src/main/java/com/rqy/controller/device/DeviceContorller.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
